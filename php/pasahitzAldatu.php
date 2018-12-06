ddbad79849d66a3869934f11f34d2a655d1d2133
zmutu <43672768+zmutu@users.noreply.github.com>
2018-12-06 10:14:33 +0100

    Update pasahitzAldatu.php

--- php/pasahitzAldatu.php
+++ php/pasahitzAldatu.php
@@ -1,5 +1,7 @@
 <?php session_start();
+
 $f = fopen('fitx.txt','a+');
+
 if(isset($_SESSION['rola'])){
 	if(strcmp($_SESSION['rola'],'kudeatzaile') != 0 && strcmp($_SESSION['rola'],'ikasle') != 0){
 		fwrite($f,"\npasahitzAldatu: erabiltzaile mota ez egokia");
@@ -11,29 +13,31 @@ else{
 	fwrite($f,"\npasahitzAldatu: saioa hasi gabe");
 	header('location:../layout.htm');
 }
-if(isset($_POST['id'])){
-	
-	fwrite($f,"\npasahitzAldatu: datoak jaso dira | id: ".substr($i,2,strlen($i)));
+if(isset($_POST['pasahitza0'])){
+
+	fwrite($f,"\npasahitzAldatu(18): datoak jaso dira | id: ".$_SESSION['id']);
 
 	include('dbConfig.php');
+
 	$konexioa = new mysqli($zerbitzaria,$erabiltzaile,$gakoa,$db);
+
 	if($konexioa -> connect_error){
-		fwrite($f,"\npasahitzAldatu: konexioa ezin izan da lortu");
+		fwrite($f,"\npasahitzAldatu(25): konexioa ezin izan da lortu");
 		echo('ezin izan da pasahitza aldatu');
 		exit();
 	}
 	$msg = datoak_aztertu($_POST['pasahitza0'],$_POST['pasahitza1'],$_POST['pasahitza2']);
 	if($msg != ''){
-		fwrite($f,"\npasahitzAldatu: ".$msg);
+		fwrite($f,"\npasahitzAldatu(31): ".$msg);
 		echo($msg);
 		exit();
 	}
-	$msg = pasahitza_gorde($konexioa,$_POST['pasahitza0'],$_POST['pasahitza1'],$_POST['id']);
+	$msg = pasahitza_gorde($konexioa,$_POST['pasahitza0'],$_POST['pasahitza1'],$_SESSION['id']);
 
-	fwrite($f,"\npasahitzAldatu: ".$msg);
+	fwrite($f,"\npasahitzAldatu(37): ".$msg);
 
 	echo($msg);
-	exti();
+	exit();
 }
 else{
 	fwrite($f,"\nez da POST datorik jaso");
@@ -45,7 +49,42 @@ input{margin:1px;}
 #logF input:invalid{border:1px solid red;}
 </style>
 <script>
-
+var hasi = {
+	onReady: function(e){
+		$('#pasahitzaF').on('submit',function(e){
+			$('*').css({cursor:'wait'});
+			e.preventDefault();
+			$.ajax({
+				type: 'POST',
+				url: 'pasahitzAldatu.php',
+				data: new FormData(this),
+				contentType: false,
+				cache: false,
+				processData: false,
+				dataType: 'text',
+				success: function(em){
+					$('*').css({cursor:'default'});
+					console.log('ongi: '+em);
+					m = $('#msg');
+					m.html(em);
+					w = $(window);
+					m.css({top:(w.height()-m.height())/2,lef:(w.width()-m.width())/2}).show();
+					setTimeout(function(){m.fadeOut();},5000);
+				},
+				error: function(er){
+					$('*').css({cursor:'default'});
+					console.log('errorea: '+er);
+					m = $('#msg');
+					m.html(er);
+					w = $(window);
+					m.css({top:(w.height()-m.height())/2,lef:(w.width()-m.width())/2}).show();
+					setTimeout(function(){m.fadeOut();},5000);
+				}
+			});
+		});
+	}
+};
+$(document).ready(hasi.onReady);
 </script>
 <div id='aldatuPSW'>
 	<span id='msg'></span><br/>
@@ -54,7 +93,6 @@ input{margin:1px;}
 			<h3 style='text-align:center;'>Pasahitza Aldatu</h3>
 			<p><label><?php $_SESSION['user']?></label></p>
 			<p>
-				<label><input type='hidden' id='id<?php echo($_SESSION['id']);?>' value='<?php echo($_SESSION['id']);?>'/></label>
 				<label>Pasahitza zaharra*: <INPUT TYPE='password' NAME='pasahitza0' id='pasahitza0' minlength='8' size='20' required></label><br/>
 				<label>Pasahitza berria*: <INPUT TYPE='password' NAME='pasahitza1' id='pasahitza1' minlength='8' size='20' required></label><br/>
 				<label>Pasahitza berria*: <INPUT TYPE='password' NAME='pasahitza2' id='pasahitza2' minlength='8' size='20' required></label><br/>
@@ -66,64 +104,6 @@ input{margin:1px;}
 		</fieldset>
 	</form>
 </div>
-<script>
-var hasi = {
-	onReady: function(e){
-		$('#pasahitzaF').on('submit',function(e){
-			$('body').css({cursor:'wait'});
-			e.preventDefault();
-			$('body').css({cursor:'wait'});
-			$.post(
-				'pasahitzAldatu.php',
-				{id:$('#pasahitza0').attr('id').substr(2,$('#pasahitza0').attr('id').length),pasahitza0:$('#pasahitza0').attr('id')),pasahitza0:$('#pasahitza0').attr('id'),pasahitza0:$('#pasahitza0').attr('id')},
-				//data:new FormData(this),
-				function(em){
-					$('body').css({cursor:'default'});
-					console.log('ongi: '+em);
-					m = $('#msg');
-					m.html(em);
-					w = $(window);
-					m.css({top:(w.height()-m.height())/2,lef:(w.width()-m.width())/2}).show();
-					setTimeout(function(){m.fadeOut();},5000);
-				}
-			);
-		}
-	}
-/*
-		$('#pasahitzaF').on('submit',function(e){
-			$('body').css({cursor:'wait'});
-			e.preventDefault();
-			$.ajax({
-				type: 'POST',
-				url: 'pasahitzAldatu.php',
-				data: new FormData(this),
-				contentType: false,
-				cache: false,
-				processData: false,
-				dataType: 'text',
-				success: function(em){
-					$('body').css({cursor:'default'});
-					console.log('ongi: '+em);
-					m = $('#msg');
-					m.html(em);
-					w = $(window);
-					m.css({top:(w.height()-m.height())/2,lef:(w.width()-m.width())/2}).show();
-					setTimeout(function(){m.fadeOut();},5000);
-				},
-				error: function(er){
-					$('body').css({cursor:'default'});
-					console.log('errorea: '+er);
-					m = $('#msg');
-					m.html(er);
-					w = $(window);
-					m.css({top:(w.height()-m.height())/2,lef:(w.width()-m.width())/2}).show();
-					setTimeout(function(){m.fadeOut();},5000);
-				}
-			});
-		});*/
-};
-$(document).ready(hasi.onReady);
-</script>
 <?php
 function datoak_aztertu($p1,$p2,$p3){
 	if(strcmp($p2,$p3) != 0){return 'pasahitz berriak ez dira berdinak';}
@@ -132,15 +112,21 @@ function datoak_aztertu($p1,$p2,$p3){
 	return '';
 }
 function pasahitza_gorde($k,$p1,$p2,$i){
-	$i = substr($i,2,strlen($i));
+
 	$r = $k -> query("Select pasahitza from users where id = '" . $i . "'");
 	$p0 = $r -> fetch_array(MYSQLI_ASSOC);
 
 	if(strcmp($p1,$p0['pasahitza']) != 0){
-		return 'pasahitz zaharra ez dago ongi';
+		return 'pasahitz zaharra ez dago ongi<br/>'.$p1.' /= '.$p0['pasahitza'].'<br/>Select pasahitza from users where id = "'.$i.'"';
 	}
-	$p = password_hash($p2, PASSWORD_DEFAULT);
-	$msg = $konexioa -> query("update users set pasahitza = '" . $p . "' where id = '" . $i . "'");
-	
+
+	$msg = $k -> query("update users set pasahitza = '" . $p2 . "' where id = '" . $i . "'");
+	sleep(2);
+	if($msg!=0){
+		return('pasahitza gorde da');
+	}
+	else{
+		return 'pasahitza gordetzean erroreren bat gertatu da';
+	}
 }
 ?>
